f71d830873a24ddd5cace082f50abf6cf88980ee
qian-xiaosheng <1326598206@qq.com>
2020-03-09 14:50:13 +0800

    修改了控制器

--- src/main/java/com/yida/firstgitdemo/controller/IndexController.java
+++ src/main/java/com/yida/firstgitdemo/controller/IndexController.java
@@ -1,10 +1,13 @@
 package com.yida.firstgitdemo.controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
+    @GetMapping("/index")
     public String index(Model model) {
         return "hello world";
     }
