5402c4e53d9060184dcc58d769d81c89f10dc939
qian-xiaosheng <1326598206@qq.com>
2020-03-09 14:15:43 +0800

    第一次提交

---
+++ src/main/java/com/yida/firstgitdemo/FirstGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yida.firstgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstGitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yida/firstgitdemo/FirstGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yida.firstgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
